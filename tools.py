262ffd79c9c0eca4d1fd3f24e2cd565bb2f445f0
Elliot Wadge <elliotwadge@gmail.com>
2022-03-11 07:25:16 +0000

    end of day 4

--- tools.py
+++ tools.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from scipy.stats import mode
 from PIL import Image
 from scipy.optimize import curve_fit
+from dataclasses import dataclass
 
 
 def local_max(arr, N = 2, strict = False):
@@ -105,9 +107,10 @@ def error_prop(f,args,errors,ind_var = [None],**kwargs):
 
 
 
-def line_intensity(filename, rows = [], cols = []):
+def line_intensity(filename, rows = [], cols = [], conversion = None, norm = False):
     '''takes filename and the rows and cols of the image you wish to inspect and returns the line intensity
-    averaged across the columns'''
+    averaged across the columns, conversion is the mm/px and setting a value will return a corresponding x array 
+    with the line profile in form x, line'''
     #get image and change to np.array
     img = np.array(Image.open(filename))
 
@@ -119,8 +122,78 @@ def line_intensity(filename, rows = [], cols = []):
         rows = [0, len(img)]
 
     
-    selected_rectangle = img[rows[0]:rows[1],cols[0]:cols[1]]
 
-    line_intensity = np.mean(selected_rectangle, axis = 0)
     
-    return line_intensity
+    selected_rectangle = img[rows[0]:rows[1],cols[0]:cols[1]]
+    line_intensity = np.mean(selected_rectangle, axis = 0)
+
+    if norm:
+        line_intensity /= np.max(line_intensity)
+
+    if conversion is not None:
+        x  = np.arange(*cols, dtype = float)
+        x += conversion*rows[0]
+        x *= conversion
+        return x, line_intensity
+
+    else:
+        return line_intensity
+
+@dataclass
+class Extracted_Data:
+    '''stores raw, the rows averages, the x in mm, the profile - the minimum and the norm_profile'''
+    raw: np.array = np.array([])
+    rows: int = 0
+    x: np.array = np.array([])
+    profile: np.array = np.array([])
+    norm_profile: np.array = np.array([])
+    err: np.array = np.array([])
+
+def extract_data(filename, conversion, rows = [], cols = [],sigmaRaw = 0.0315, norm = 0):
+    '''extracts data and returns the data in Extracted_Data class'''
+
+    #load in greyscale image
+    raw = np.array(Image.open(filename))
+    
+    #handle case where no rows or cols are specified
+    if len(cols) == 0:
+        cols = [0, len(raw[0])]
+    
+    if len(rows) == 0:
+        rows = [0, len(raw)]
+    
+    #select rectangle
+    selected_rectangle = raw[rows[0]:rows[1],cols[0]:cols[1]]
+
+    #get profile of rectangle
+    profile = np.mean(selected_rectangle, axis = 0)
+    profileMin = np.min(profile)
+    rowsAveraged = rows[1] - rows[0]
+    
+    _, index = local_max(profile, N = len(profile))
+
+    #get the position from center of sensor
+    x  = np.arange(*cols, dtype = float)
+    x += conversion*rows[0] - np.median(x[index])
+    x *= conversion
+
+    # Calculate error in profile (ydatea)
+    GreyValMax = 255
+    SensorWellMax = 15585 # according to http://softwareservices.flir.com/BFS-PGE-16S2/latest/EMVA/EMVA.html
+    alpha_1 = np.sqrt( profile*GreyValMax/(SensorWellMax*rowsAveraged) ) # poisson statistics of pixels capturing N photons
+    alpha_0 = np.ones(len(x))*sigmaRaw                             # baseline dark + ambient uncertainty
+    y_error = alpha_0 + alpha_1
+
+    # subtract minimum baseline light from profile
+    profile -= profileMin
+
+    if norm == 'abs': # in 8 bit
+        norm_profile = profile/GreyValMax
+    else: # on axis normalization
+        norm_profile = profile/np.max(profile)
+
+
+    
+
+    return Extracted_Data(raw = raw, profile = profile, norm_profile = norm_profile, x = x, rows = rowsAveraged, err = y_error)
+
